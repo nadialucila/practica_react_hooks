b0a34dd5b8d8587436804983d252d34122e46871
nadialucila <nadia.bergara17@gmail.com>
2022-11-13 22:29:39 -0300

    aprendiendo useEffect

---
+++ src/components/useEffect/SimpleForm.js
@@ -0,0 +1,63 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import './effects.css';
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+
+    const {name, email} = formState;
+
+    useEffect( () => {
+      console.log("Hola!");
+      //este useEffect se dispara cuando carga la pagina nomas
+    },[]);
+
+    useEffect( () => {
+        //este useEffect se dispara cuando cambia el formState
+        console.log("Chaus!");
+    },[formState]);
+
+    useEffect( () => {
+    //este useEffect se dispara cuando cambia el email
+    console.log("aaaa!");
+    },[email]);
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+    return (
+        <Fragment>
+            <h1> UseEffect </h1>
+            <hr/>
+
+            <div className='form-group'>
+                <input
+                    type='text'
+                    name='name'
+                    className='form-control'
+                    placeholder='Nombre'
+                    autoComplete='off'
+                    value={name}
+                    onChange={ handleInputChange }
+                />
+            </div>
+            <div className='form-group'>
+                <input
+                    type='text'
+                    name='email'
+                    className='form-control'
+                    placeholder='Email'
+                    autoComplete='off'
+                    value={email}
+                    onChange={ handleInputChange }
+                />
+            </div>
+        </Fragment>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SimpleForm } from './components/useEffect/SimpleForm';
 // import { CounterApp } from './components/useState/CounterApp';
-import { CounterCustomHook } from './components/useState/CounterCustomHook';
+// import { CounterCustomHook } from './components/useState/CounterCustomHook';
 // import { HooksApp } from './HooksApp';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounterCustomHook />
+    <SimpleForm />
   </React.StrictMode>
 );
 
