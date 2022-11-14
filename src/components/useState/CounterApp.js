0d465d42bac62c240b83fb09fd81af5f64450ede
nadialucila <nadia.bergara17@gmail.com>
2022-11-13 21:12:49 -0300

    practicas hook useState

--- src/components/useState/CounterApp.js
+++ src/components/useState/CounterApp.js
@@ -1,14 +1,35 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import './counter.css';
 
 export const CounterApp = () => {
-  return (
-    <Fragment>
-        <h1>Counter {0}</h1>
-        <hr/>
 
-        <button className='btn btn-primary'>
-            +1
-        </button>
-    </Fragment>
-  )
+    // const [counter, setCounter] = useState(10);
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 45,
+        counter4: 55
+    })
+
+    const {counter1, counter2} = state;
+
+    return (
+        <Fragment>
+            {/* <h1>Counter {counter}</h1> */}
+            <h1>Counter 1 {counter1}</h1>
+            <h1>Counter 2 {counter2}</h1>
+            <hr/>
+
+            <button className='btn btn-primary' onClick={ () => { 
+                // setCounter(counter+1)
+                setState({
+                    //aca dejo todos los demas estados como estaban y solo cambio el que me interesa.
+                    ...state,
+                    counter1: counter1+1
+                })
+             }}>
+                +1
+            </button>
+        </Fragment>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp';
+import { CounterApp } from './components/useState/CounterApp';
+// import { HooksApp } from './HooksApp';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HooksApp />
+    <CounterApp />
   </React.StrictMode>
 );
 
